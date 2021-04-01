810ab97648bfc562f64b962dd61f166d7194a0e6
Martin Lazarov <martin@example.com>
2021-04-01 13:57:03 +0300

    feat: display asks for symbol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { webSocket, WebSocketSubject } from "rxjs/webSocket";
 import { startWith, map } from 'rxjs/operators';
 
 export interface IBookOrder {
-  subject: WebSocketSubject<any>;
   symbol: string;
   label: string;
 }
@@ -20,40 +19,57 @@ export class AppComponent {
   filteredOptions: Observable<IBookOrder[]>;
   altcoins: IBookOrder[] = [];
 
-  displayedColumns: string[] = ['price', 'amount', 'total'];
-  dataSource = [];
+  displayedColumns: string[] = ['price', 'quantity', 'total'];
+  dataSourceAsks = [];
+  dataSourceBids = [];
+  stream: WebSocketSubject<any>;
 
   constructor() {
     this.altcoins = [{
-      subject: null,
       symbol: 'btcusdt',
       label: 'BTC/USDT'
     }, {
-      subject: null,
       symbol: 'ethbtc',
       label: 'ETH/BTC'
     }];
-    this.altcoins.forEach((coin) => {
+    // this.altcoins.forEach((coin) => {
 
-      coin.subject = webSocket(`wss://stream.binance.com:9443/ws/${coin.symbol}@depth10@100ms`);
 
-      coin.subject.subscribe(
-        data => {
-          console.log('>>>data', data)
-        }, // Called whenever there is a message from the server.
-        err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
-        () => console.log('complete') // Called when connection is closed (for whatever reason).
-      );
-      setTimeout(() => {
-        coin.subject.unsubscribe();
-      }, 3500)
-    })
+
+    //   setTimeout(() => {
+    //     coin.subject.unsubscribe();
+    //   }, 3500)
+    // })
   }
 
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => this._filter(value))
+    );
+  }
+
+  onSymbolSelect(option: IBookOrder): void {
+    if (this.stream) {
+      this.stream.unsubscribe();
+      this.stream = null;
+    }
+    this.stream = webSocket(`wss://stream.binance.com:9443/ws/${option.symbol}@depth10@1000ms`);
+    this.stream.subscribe(
+      ({ asks, bids }) => {
+        console.log('>>>data', asks, bids)
+        this.dataSourceAsks = asks.map((row: string[]) => {
+          const price = parseFloat(row[0]);
+          const quantity = parseFloat(row[1]);
+          return {
+            price: price.toFixed(6),
+            quantity: quantity.toFixed(3),
+            total: (price * quantity).toFixed(5)
+          }
+        })
+      }, // Called whenever there is a message from the server.
+      err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+      () => console.log('complete') // Called when connection is closed (for whatever reason).
     );
   }
 
